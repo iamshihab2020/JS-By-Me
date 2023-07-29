2e50fc249c86079fb1ef8e40ad9e9286ed48f604
Sheikh Shihab Hossain <meet2shihab18@gmail.com>
2023-07-30 02:45:28 +0600

    modified:   .vscode/launch.json 	modified:   Project-7-digital-clock-timer/app.js 	modified:   Project-7-digital-clock-timer/index.html 	modified:   Project-7-digital-clock-timer/style.css 	new file:   Project-8-slider_basic/app.js 	new file:   Project-8-slider_basic/index.html 	new file:   Project-8-slider_basic/style.css 	new file:   Project-9-image_slider_2/app.js 	new file:   Project-9-image_slider_2/img/a1.jpg 	new file:   Project-9-image_slider_2/img/a2.jpg 	new file:   Project-9-image_slider_2/img/a3.jpg 	new file:   Project-9-image_slider_2/img/a4.jpg 	new file:   Project-9-image_slider_2/index.html 	new file:   Project-9-image_slider_2/style.css

--- Project-7-digital-clock-timer/app.js
+++ Project-7-digital-clock-timer/app.js
@@ -1,4 +1,9 @@
-const updateTime(){
+const addTrailingZer = (num) => {
+    return num < 10 ? "0" + num : num
+}
+
+
+const updateTime = () => {
     const time = new Date()
     let hours = time.getHours(),
     minutes = time.getMinutes(),
@@ -14,9 +19,49 @@ const updateTime(){
     minutes = addTrailingZero(minutes)
     seconds = addTrailingZero(seconds)
 
-}
+    $("#hour").html(hours)
+    $("#min").html(minutes)
+    $("#sec").html(seconds)
+    $("#ampm").html(ampm)
+    $("#other-ampm").html(otherampm)
 
-const addTrailingZer = (num) => {
-    return num < 10 ? "0" + num : num
 }
+// cll the function on page load
+updateTime()
 
+// call function after every second
+
+setInterval(updateTime, 1000)
+
+
+// stopwatch
+let stopWatchHours = 0,
+stopWatchMinutes = 0,
+stopWatchSeconds = 0,
+stopwatchMiliSeconds = 0,
+stopWatchRunning = 0
+laps = 0,
+stopWatchInterval
+
+
+const stopwatch = () =>{
+    // increase millisecond
+    stopwatchMiliSeconds++
+
+    if(stopwatchMiliSeconds === 100){
+        // if stopwatch milisecond equals 100 increase one sec and set ms 0
+        stopWatchSeconds++
+        stopWatchSeconds = 0
+    }
+
+    if(stopWatchSeconds === 60){
+        stopWatchMinutes++
+        stopWatchSeconds = 0
+    }
+
+
+    if(stopWatchMinutes === 60){
+        stopWatchHours++
+        stopWatchMinutes = 0
+    }
+}
---
+++ Project-8-slider_basic/app.js
@@ -0,0 +1,25 @@
+const slide = document.querySelectorAll('.slide')
+var counter = 0
+
+
+slide.forEach( (slide,index) =>{
+    slide.style.left = `${index * 100}%`
+})
+
+
+const goNext = ()=>{
+    counter++
+    slideImage()
+}
+
+const goPrev = ()=>{
+    counter--
+    slideImage()
+}
+
+
+const slideImage = () =>{
+    slide.forEach((slide) =>{
+        slide.style.transform = `translateX(-${counter*100}%)`
+    })
+}
