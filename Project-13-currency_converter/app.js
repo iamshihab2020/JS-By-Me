ee464f6a523d9f8f7bae16c094d20babbae22390
Sheikh Shihab Hossain <meet2shihab18@gmail.com>
2023-09-07 13:52:09 +0600

    new file:   Project-13-currency_converter/app.js 	new file:   Project-13-currency_converter/currencyData.js 	new file:   Project-13-currency_converter/index.html 	new file:   Project-13-currency_converter/style.css

---
+++ Project-13-currency_converter/app.js
@@ -0,0 +1,19 @@
+const convertFrom = document.querySelector('#convertFrom'),
+convertMoney = document.querySelector('#convertMoney'),
+convertTo = document.querySelector('#convertTo'),
+convert = document.querySelector('#convert'),
+con = document.querySelector('#con'),
+con2 = document.querySelector('#con2'),
+conForm = document.querySelector('#conForm'),
+conTo = document.querySelector('#conTo') ;
+convert.addEventListener('click',()=>{
+    let convertFromValue = convertFrom.value;
+    let convertFromMoney = convertMoney.value;
+    let convertToValue = convertTo.value;
+    let count = `${allCurrency[`${convertFromValue}`][`${convertToValue}`]}`;
+    let result = `${convertFromMoney}` * `${count}`;
+    con.value = convertFromMoney;
+    conForm.innerHTML = convertFromValue;
+    con2.value = result;
+    conTo.innerHTML = convertToValue;
+});
---
+++ Project-13-currency_converter/currencyData.js
@@ -0,0 +1,31 @@
+let allCurrency = {
+    'taka': {
+        'taka': 1,
+        'dollar' : 0.0091,
+        'euros' : 0.0085,
+        'yuan' : 0.067,
+        'rupees' : 0.76
+    },
+    'rupees': {
+        'rupees': 1,
+        'dollar' : 0.012,
+        'euros' : 0.011,
+        'yuan' : 0.88,
+        'taka' : 1.32
+    },
+    'dollar': {
+        'dollar': 1,
+        'rupees' : 83.16,
+        'euros' : 0.93,
+        'yuan' : 7.32,
+        'taka' : 109.97
+    },
+    'yuan': {
+        'yuan' : 1,
+        'rupees' : 11.35,
+        'euros' : 0.13,
+        'dollar' : 0.14,
+        'taka' : 15.01
+    }
+}
+
